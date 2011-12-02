e04e1ee112e914de105803ba09699052f3bd93f3
Robert Larsen <robert@the-playground.dk>
2011-12-02 20:38:35 +0100

    Implemented the getAllNeighbours method on cell.

--- src/circmaze.js
+++ src/circmaze.js
@@ -17,11 +17,20 @@ CircMaze.prototype.eachCell = function(callback, obj) {
     });
 };
 
-CircMaze.Cell = function(id, ring, startAngle, endAngle) {
+CircMaze.Cell = function(id, ring, idx, startAngle, endAngle) {
     this._id = id;
     this._ring = ring;
+    this._idx = idx;
     this._startAngle = startAngle;
     this._endAngle = endAngle;
+    this._neighbours = [];
+};
+
+CircMaze.Cell.prototype.toString = function() {
+    var toD = function(r) {
+        return r / Math.PI * 180;
+    }
+    return "(" + this._id + "," + this._ring._nr + "," + toD(this._startAngle) + "-" + toD(this._endAngle) + ")";
 };
 
 CircMaze.Cell.prototype.getId = function() {
@@ -38,7 +47,22 @@ CircMaze.Cell.prototype.paint = function(context) {
 
 CircMaze.Cell.prototype.hasAllWalls = function() { };
 CircMaze.Cell.prototype.breakWallTo = function(neighbour) { };
-CircMaze.Cell.prototype.getAllNeighbours = function() { };
+
+CircMaze.Cell.prototype.getAllNeighbours = function() {
+    if (this._neighbours.length === 0) {
+        var n = this._ring._cells.length;
+        this._neighbours.push(this._ring._cells[(this._idx + 1) % n]);
+        this._neighbours.push(this._ring._cells[(this._idx + n - 1) % n]);
+        if (this._ring._nr > 0) {
+            this._neighbours = this._neighbours.concat(this._ring._maze._rings[this._ring._nr - 1].getCellsInArc(this._startAngle, this._endAngle));
+        }
+        if (this._ring._nr < this._ring._maze._rings.length - 1) {
+            this._neighbours = this._neighbours.concat(this._ring._maze._rings[this._ring._nr + 1].getCellsInArc(this._startAngle, this._endAngle));
+        }
+    }
+
+    return this._neighbours;
+};
 
 CircMaze.Ring = function(maze, nr, radius) {
     this._maze = maze;
@@ -53,7 +77,7 @@ CircMaze.Ring = function(maze, nr, radius) {
     this._cells = new Array(num);
 
     for (var i = 0; i < num; i++) {
-        this._cells[i] = new CircMaze.Cell(lastId + i, this, i * arc, (i + 1) * arc);
+        this._cells[i] = new CircMaze.Cell(lastId + i, this, i, i * arc, (i + 1) * arc);
     }
 };
 
@@ -76,4 +100,24 @@ CircMaze.Ring.prototype.calculateNumCells = function() {
         num = (doubleCells <= num ? doubleCells : neighbourCircle._cells.length);
     }
     return num;
+};
+
+CircMaze.Ring.prototype.getCellsInArc = function(startAngle, endAngle) {
+    if (startAngle > endAngle) {
+        endAngle += Math.PI * 2;
+    }
+
+    var c = [];
+    this._cells.forEach(function(cell) {
+        var sa = cell._startAngle;
+        var ea = cell._endAngle;
+        if (sa > ea) {
+            ea += Math.PI * 2;
+        }
+
+        if (ea > startAngle && sa < endAngle) {
+            c.push(cell);
+        }
+    });
+    return c;
 };
